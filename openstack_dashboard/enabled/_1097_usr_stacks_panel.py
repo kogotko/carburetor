dd4ddf53a8ebe960983e10632c319d7eb9e7f4b7
kogotko <alexandrov.e.v@gmail.com>
2017-07-02 03:45:46 +0300

    +3 int

---
+++ openstack_dashboard/enabled/_1097_usr_stacks_panel.py
@@ -0,0 +1,9 @@
+# The slug of the panel to be added to HORIZON_CONFIG. Required.
+PANEL = 'stacks'
+# The slug of the dashboard the PANEL associated with. Required.
+PANEL_DASHBOARD = 'usr'
+# The slug of the panel group the PANEL is associated with.
+PANEL_GROUP = 'usrorc'
+
+# Python panel class of the PANEL to be added.
+ADD_PANEL = 'openstack_dashboard.dashboards.usr.stacks.panel.Stacks'
